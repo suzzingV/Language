ac435a58b1d40c34014d6eee31e751c665f17a22
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-11-29 21:25:11 +0900

    접근 제한자

---
+++ java20221129/src/sec13/exam01/package1/B.java
@@ -0,0 +1,5 @@
+package sec13.exam01.package1;
+
+public class B { //public 접근제한자: 다 사용 가능
+    A a; //같은 패키지안에 있으므로 A 사용 가능
+}
---
+++ java20221129/src/sec13/exam01/package2/C.java
@@ -0,0 +1,8 @@
+package sec13.exam01.package2;
+
+import sec13.exam01.package1.*;
+
+public class C {
+    A a; //다른 패키지이므로 쓸 수 없음
+    B b;
+}
