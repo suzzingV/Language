45484eeacfaca78aea4f98cdb2c0530fa427a97a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-11-29 23:48:11 +0900

    주석 수정

--- java20221129/src/sec15/exam01/PrintAnnotationExample.java
+++ java20221129/src/sec15/exam01/PrintAnnotationExample.java
@@ -7,7 +7,7 @@ public class PrintAnnotationExample {
         Method[] declaredMethods = Service.class.getDeclaredMethods(); //Service클래스에 선언된 메소드 얻기(리플렉션)
 
         for(Method method : declaredMethods) {
-            if(method.isAnnotationPresent(PrintAnnotation.class)) { //지정한 어노테이션이 적용되어 있으면 어노테이션 리턴, 아니면 null리턴
+            if(method.isAnnotationPresent(PrintAnnotation.class)) { //지정한 어노테이션이 적용되어 있는지 여부
                 PrintAnnotation printAnnotation = method.getAnnotation(PrintAnnotation.class); //어노테이션 객체 얻기
                 
                 System.out.println("[" + method.getName() + "]");
