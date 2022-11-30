c99c5839280d395c52bc68dd1510344919740ed6
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-12-01 00:27:00 +0900

    .

---
+++ 20220930/ft_putchar.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void ft_putchar(char z)
+{
+	printf("%c", z);
+}
---
+++ 20220930/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern void rush(int x, int y);
+extern int area_id(int b, int a, int i, int j);
+
+int main(void)
+{
+	rush(5, 5);
+	return 0;
+}
