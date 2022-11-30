c62a8396a7789eda6a2327ccd7222ac13e72f5bc
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-11-30 23:53:43 +0900

    오버라이딩

--- java20221130/src/sec04/Computer.java
+++ java20221130/src/sec04/Computer.java
@@ -1,7 +1,7 @@
 package sec04;
 
 public class Computer extends Calculator {
-    @Override //안 붙여도 되지만 붙이면 더 정확
+    @Override //안 붙여도 되지만 붙이면 더 정확하게 오버라이딩 된 것인지 컴파일러가 체크
     double areaCircle(double r) {
         System.out.println("Computer 객체의 areaCircle() 실행");
         return Math.PI * r * r;
