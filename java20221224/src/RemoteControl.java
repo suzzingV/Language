69df4e4efe2efaab76d741189d5fc64393210f52
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-12-24 17:24:44 +0900

    디폴트 메소드

--- java20221224/src/RemoteControl.java
+++ java20221224/src/RemoteControl.java
@@ -7,4 +7,13 @@ public interface RemoteControl {
     public void turnOff();
     public void setVolume(int volume);
     //추상메소드 선언 //public abstract 안붙여도 자동으로 붙음 //중괄호 없는
+
+    //디폴트 메소드
+    default void setMute(boolean mute) {
+        if(mute) {
+            System.out.println("무음 처리합니다.");
+        } else {
+            System.out.println("무음 해제합니다.");
+        }
+    }
 }
