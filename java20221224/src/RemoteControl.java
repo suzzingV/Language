882ca1acf0ec9a17527a1a597e50989f53d8695a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-12-24 17:27:09 +0900

    인터페이스 정적 메소드 선언

--- java20221224/src/RemoteControl.java
+++ java20221224/src/RemoteControl.java
@@ -8,12 +8,17 @@ public interface RemoteControl {
     public void setVolume(int volume);
     //추상메소드 선언 //public abstract 안붙여도 자동으로 붙음 //중괄호 없는
 
-    //디폴트 메소드
+    //디폴트 메소드 //public default: public 안붙여도 자동으로 컴파일 과정에서 붙음 //실행내용 작성
     default void setMute(boolean mute) {
         if(mute) {
             System.out.println("무음 처리합니다.");
         } else {
             System.out.println("무음 해제합니다.");
         }
+    }
+
+    //정적 메소드 //객체 없어도 실행 가능
+    static void changeBattery() { 
+        System.out.println("건전지를 교환합니다.");
     }
 }
