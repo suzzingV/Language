b7604e0ca1336e078ddb140d0027e0b3b5c97220
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-12-25 15:32:49 +0900

    인터페이스 정적 메소드 사용

---
+++ java20221225/src/sec04/exam03/RemoteControlExample.java
@@ -0,0 +1,9 @@
+package sec04.exam03;
+
+import sec04.exam01.*;
+
+public class RemoteControlExample {
+    public static void main(String[] args) {
+        RemoteControl.changeBattery(); //인터페이스의 정적 메소드는 객체 없어도 바로 사용 가능
+    }
+}
