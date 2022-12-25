44dd7d1890d88f0676b6920476b0caac0a333dfd
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-12-25 15:26:12 +0900

    디폴드 메소드 재정의

---
+++ java20221225/src/sec04/exam02/Audio.java
@@ -0,0 +1,36 @@
+package sec04.exam02;
+
+import sec04.exam01.*;
+public class Audio implements RemoteControl {
+    private int volume;
+    private boolean mute;
+
+    public void turnOn() {
+        System.out.println("Audio를 켭니다.");
+    }
+
+    public void turnOff() {
+        System.out.println("Audio를 끕니다.");
+    }
+
+    public void setVolume(int volume) {
+        if(volume > RemoteControl.MAX_VOLUME) {
+            this.volume = RemoteControl.MAX_VOLUME;
+        } else if(volume < RemoteControl.MIN_VOLUME) {
+            this.volume = RemoteControl.MIN_VOLUME;
+        } else {
+            this.volume = volume;
+        }
+        System.out.println("현재 Audio 볼륨: " + this.volume);
+    }
+
+    @Override //디폴트 메소드 재정의
+    public void setMute(boolean mute) {
+        this.mute = mute;
+        if(mute) {
+            System.out.println("Audio 무음 처리합니다.");
+        } else {
+            System.out.println("Audio 무음 해제합니다.");
+        }
+    }
+}
