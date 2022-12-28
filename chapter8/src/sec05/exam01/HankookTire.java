fe8beb468a8b3b86b1ba591b5aed405b0c1feee6
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-12-29 04:14:02 +0900

    최종

--- chapter8/src/sec05/exam01/HankookTire.java
+++ chapter8/src/sec05/exam01/HankookTire.java
@@ -1,6 +1,6 @@
 package sec05.exam01;
 
-public class Hankook implements Tire{
+public class HankookTire implements Tire{
     @Override
     public void roll() {
         System.out.println("한국 타이어가 굴러갑니다.");
