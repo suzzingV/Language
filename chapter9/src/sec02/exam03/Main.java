0ff08920d030415ca38949a451a139a12a83f2b6
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-12-30 21:15:35 +0900

    중첩클래스 실행

---
+++ chapter9/src/sec02/exam03/Main.java
@@ -0,0 +1,19 @@
+package sec02.exam03;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+
+        A.B b = a.new B(); //인스턴스 멤버 클래스는 바깥 클래스 만들고 접근 가능
+        b.field1 = 3;
+        b.method1();
+
+        A.C c = new A.C(); //정적 멤버 클래스는 바로 만들 수 있다
+        c.field1 = 3;
+        c.method1();
+        A.C.field2 = 3;
+        A.C.method2();
+
+        a.method();
+    }
+}
