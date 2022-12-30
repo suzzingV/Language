d9bfdc8bda8b8bf744c362d73d260456ee8c402b
suzzingv <84590065+suzzingv@users.noreply.github.com>
2022-12-30 21:17:18 +0900

    정적 필드와 메소드

--- chapter9/src/sec02/exam03/Main.java
+++ chapter9/src/sec02/exam03/Main.java
@@ -11,7 +11,7 @@ public class Main {
         A.C c = new A.C(); //정적 멤버 클래스는 바로 만들 수 있다
         c.field1 = 3;
         c.method1();
-        A.C.field2 = 3;
+        A.C.field2 = 3; //c에도 적용됨
         A.C.method2();
 
         a.method();
