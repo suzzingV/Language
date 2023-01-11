6ced7a39808c9590701167d29afbbc8be3513c80
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-01-11 16:18:03 +0900

    익명객체에서 사용된 매개변수와 로컬변수는 final

--- chapter9/src/sec05/exam03/Anonymous.java
+++ chapter9/src/sec05/exam03/Anonymous.java
@@ -7,6 +7,13 @@ public class Anonymous {
         final int var1 = 0;
         int var2 = 0;
 
+        //var1 = 10;
+        //var2 = 10;
+        //arg1 = 10;
+        //arg2 = 10;
+        //final 붙든 안 붙든 익명객체에서 사용된 매개변수와 로컬변수는 final 특성 가진다
+        //final 키워드 있으면 메소드 내부에 지역변수로 복사됨
+        //final 키워드 없으면 익명클래스의 필드로 복사됨
         field = 10;
 
         Calculatable calc = new Calculatable() {
