9498a244829da4b26eda9b02b69b96e34ca95b8a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-01-18 12:38:56 +0900

    설명

--- chapter11/src/sec03/exam05/FinalizeExample.java
+++ chapter11/src/sec03/exam05/FinalizeExample.java
@@ -7,7 +7,9 @@ public class FinalizeExample {
             counter = new Counter(i);
 
             counter = null;
-            System.gc(); //쓰레기 수집가 실행요청 //메모리 상태 보고 무작위로 실행됨
+            System.gc(); //쓰레기 수집기 실행요청 //메모리 상태 보고 무작위로 실행됨
+            //한두번의 객체 쓰레기로 만들었다고 수집기 실행되는 거 아님
+            //메모리 부족, cpu한가할 때 jvm에 의해 실행됨
         }
     }
 }
