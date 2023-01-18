58628659a7f621f5fbb940e75545758ee53847d9
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-01-18 12:33:54 +0900

    finalize 메소드 재정의

---
+++ chapter11/src/sec03/exam05/Counter.java
@@ -0,0 +1,14 @@
+package sec03.exam05;
+
+public class Counter {
+    public int no;
+
+    public Counter(int no) {
+        this.no = no;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println(no + "번 객체의 finalize()가 실행됨");
+    }
+}
