35e84e2cdaa6772fdaeca40ded25129d4a6e445b
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-01-20 05:45:12 +0900

    설명추가

--- chapter11/src/sec04/exam01/CompareExample.java
+++ chapter11/src/sec04/exam01/CompareExample.java
@@ -11,6 +11,7 @@ public class CompareExample {
 
         int result = Objects.compare(s1, s2, new StudentComparator());
         //Objects에 compare 메소드 있는데 그 메소드는 세번째 인자의 인터페이스의 compare 메소드를 이용해 객체 둘을 비교하는 메소드
+        //인터페이스에는 제네릭 인터페이스 타입으로 두 객체를 비교하는 compare(T a, T b) 메소드 정의되어 있다.
         //같으면 0, 앞에게 뒤에게보다 작으면 음수, 크면 양수
         System.out.println(result);
         result = Objects.compare(s1, s3, new StudentComparator());
