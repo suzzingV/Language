b9066e2355dee770cc7abb5b7fa33da31ee4451b
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-01-21 23:14:36 +0900

    리턴값 설명

--- chapter11/src/sec04/exam02/EqualsAndDeepEqualsExample.java
+++ chapter11/src/sec04/exam02/EqualsAndDeepEqualsExample.java
@@ -14,7 +14,7 @@ public class EqualsAndDeepEqualsExample {
         System.out.println(Objects.equals(null, null));
         System.out.println(Objects.deepEquals(o1, o2) + "\n"); //배열 아닌 것은 equals랑 리턴값 똑같음
 
-        Integer[] arr1 = { 2, 1 };
+        Integer[] arr1 = { 1, 2 };
         Integer[] arr2 = { 1, 2 };
         System.out.println(Objects.equals(arr1, arr2)); //번지수 다르므로 다른 배열
         System.out.println(Objects.deepEquals(arr1, arr2)); //Objects.deepEquals(Object[] a, Object[] b) : 두 객체의 동등, 서로 다른 배열일 경우 항목 값이 같다면 true리턴, 배열 아니면 a.equals(b)의 리턴값
--- chapter11/src/sec04/exam03/HashCodeExample.java
+++ chapter11/src/sec04/exam03/HashCodeExample.java
@@ -6,8 +6,8 @@ public class HashCodeExample {
     public static void main(String[] args) {
         Student s1 = new Student(1, "홍길동");
         Student s2 = new Student(1, "홍길동");
-        System.out.println(s1.hashCode());
-        System.out.println(Objects.hashCode(s2));
+        System.out.println(s1.hashCode()); 
+        System.out.println(Objects.hashCode(s2)); //필드값 똑같으면 똑같은 값 반환
     }
 
     static class Student{
