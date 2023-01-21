875af22f31fc39dcd8fe2f939072b3121ea178dc
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-01-21 23:49:44 +0900

    부가 설명

--- chapter11/src/sec04/exam05/ToStringExample.java
+++ chapter11/src/sec04/exam05/ToStringExample.java
@@ -7,7 +7,7 @@ public class ToStringExample {
         String str1 = "홍길동";
         String str2 = null;
 
-        System.out.println(Objects.toString(str1));
+        System.out.println(Objects.toString(str1)); //Objects.toString(Object o) : null이면 null, null 아니면 o.toString()
         System.out.println(Objects.toString(str2));
         System.out.println(Objects.toString(str2, "이름이 없습니다.")); //null이면 두번째 매개값에 적힌 메시지 나옴
     }
