ecc859a88b1b2b17609b344c7c762c73220886af
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-02-12 02:30:04 +0900

    왜 바이트 수 안 맞나

--- chapter11/src/sec07/exam01/KeyBoardToStringExample.java
+++ chapter11/src/sec07/exam01/KeyBoardToStringExample.java
@@ -10,7 +10,6 @@ public class KeyBoardToStringExample {
         System.out.print("입력: ");
         int readByteNo = System.in.read(bytes); //System.in.read: 키보드에서 입력한 내용을 매개값으로 주어진 바이트 배열에 저장하고 읽은 바이트 수 리턴
         //엔터는 \r \n
-        
         String str = new String(bytes, 0, readByteNo - 2);
         System.out.println(str);
     }
