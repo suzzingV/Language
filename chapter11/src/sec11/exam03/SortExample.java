0e579f12253eb5194fff1520387aee305440d09e
sjlim1999@naver.com <sjlim1999@naver.com>
2023-02-14 01:38:30 +0900

    내림차순

--- chapter11/src/sec11/exam03/SortExample.java
+++ chapter11/src/sec11/exam03/SortExample.java
@@ -10,12 +10,12 @@ public class SortExample {
         System.out.println();
 
         String[] names = { "홍길동", "박동수", "김민수" };
-        Arrays.sort(scores);
+        Arrays.sort(names);
         System.out.println(Arrays.toString(names));
         System.out.println();
 
         Member[] members = { new Member("홍길동"), new Member("박동수"), new Member("김민수") };
-        Arrays.sort(scores);
+        Arrays.sort(members);
         System.out.print("[");
         for(int i = 0; i < members.length; i++) {
             System.out.print(members[i].name);
