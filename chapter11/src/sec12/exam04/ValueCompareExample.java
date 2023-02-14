3a04284b88f1ea4a0ad65cbd564a8ab89a5512f3
sjlim1999@naver.com <sjlim1999@naver.com>
2023-02-15 00:31:07 +0900

    포장 객체 비교

---
+++ chapter11/src/sec12/exam04/ValueCompareExample.java
@@ -0,0 +1,20 @@
+package sec12.exam04;
+
+public class ValueCompareExample {
+    public static void main(String[] args) {
+        System.out.println("[-128~127 초과값일 경우]");
+        Integer obj1 = 300;
+        Integer obj2 = 300;
+        System.out.println("==결과: " + (obj1 == obj2)); //주소값 비교
+        System.out.println("언박싱 후 결과: " + (obj1.intValue() == obj2.intValue()));
+        System.out.println("equals() 결과: " + obj1.equals(obj2));
+        System.out.println();
+
+        System.out.println("[-128~127 범위값일 경우]");
+        Integer obj3 = 10;
+        Integer obj4 = 10;
+        System.out.println("==결과: " + (obj3 == obj4)); //내부 값 비교
+        System.out.println("언박싱 후 결과: " + (obj3.intValue() == obj4.intValue()));
+        System.out.println("equals() 결과: " + obj3.equals(obj4));
+    }
+}
