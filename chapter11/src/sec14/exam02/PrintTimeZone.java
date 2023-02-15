b555cc6cf5df0ef8b597587de83987d4e67702db
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-02-15 15:31:29 +0900

    사용 가능한 시간대 문자열 출력

---
+++ chapter11/src/sec14/exam02/PrintTimeZone.java
@@ -0,0 +1,12 @@
+package sec14.exam02;
+
+import java.util.TimeZone;
+
+public class PrintTimeZone {
+    public static void main(String[] args) {
+        String[] availableIDs = TimeZone.getAvailableIDs(); //TimeZone.getAvailableIDs(): String 배열, 사용 가능한 시간대
+        for(String id : availableIDs) {
+            System.out.println(id);
+        }
+    }
+}
