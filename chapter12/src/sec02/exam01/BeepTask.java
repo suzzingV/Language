f2761b091622d5a3c829c78d0b45bea533571bac
sjlim1999@naver.com <sjlim1999@naver.com>
2023-02-18 01:43:36 +0900

    Thread 익명 객체 사용

--- chapter12/src/sec02/exam01/BeepTask.java
+++ chapter12/src/sec02/exam01/BeepTask.java
@@ -2,7 +2,7 @@ package sec02.exam01;
 
 import java.awt.*;
 
-public class BeepTask implements Runnable{
+public class BeepTask implements Runnable{ //Runnable은 인터페이스
     public void run() {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         
