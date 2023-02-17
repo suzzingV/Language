7850609ab3c983a3c00148f0327c70069c349103
sjlim1999@naver.com <sjlim1999@naver.com>
2023-02-18 00:56:41 +0900

    비프음 내기

---
+++ chapter12/src/sec02/exam01/BeepPrintExample.java
@@ -0,0 +1,19 @@
+package sec02.exam01;
+
+import java.awt.*;
+
+public class BeepPrintExample {
+    public static void main(String[] args) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+        for(int i = 0; i < 5; i++) {
+            toolkit.beep();
+            try { Thread.sleep(500);} catch(Exception e) {}
+        }
+
+        for(int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try { Thread.sleep(500); } catch(Exception e) {}
+        }
+    }
+}
