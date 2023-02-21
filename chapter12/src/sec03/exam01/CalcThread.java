7d62166f2bb4c44cde40d1a816c14b64d16cdde2
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-02-22 00:27:43 +0900

    우선 순위 설정해서 스레드 실행

--- chapter12/src/sec03/exam01/CalcThread.java
+++ chapter12/src/sec03/exam01/CalcThread.java
@@ -5,7 +5,7 @@ public class CalcThread extends Thread {
         setName(name);
     }
 
-    public run() {
+    public void run() {
         for(int i = 0; i < 2000000000; i++) {}
         System.out.println(getName());
     }
