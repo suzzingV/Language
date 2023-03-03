b0b5627fd941e3eeee5e0563b4029a49a05eb37b
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-04 01:41:32 +0900

    데몬 스레드

---
+++ chapter12/src/sec07/DaemonExample.java
@@ -0,0 +1,16 @@
+package sec07;
+
+public class DaemonExample {
+    public static void main(String[] args) {
+        AutoSaveThread autoSaveThread = new AutoSaveThread();
+        autoSaveThread.setDaemon(true); //스레드.setDaemon(true) : 데몬스레드로 만듦
+        autoSaveThread.start(); //setDaemon보다 start 먼저하면 IllegalThreadStateException 발생
+
+        try {
+            Thread.sleep(3000);
+        } catch(InterruptedException e) {
+        }
+        
+        System.out.println("메인 스레드 종료");
+    }
+}
