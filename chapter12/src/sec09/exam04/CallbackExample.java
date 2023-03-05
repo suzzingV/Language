ddd8faafa6fa739a25eb0a8eb82b774f8803a220
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-05 20:30:51 +0900

    콜백 방식의 작업 완료 통보 받기

---
+++ chapter12/src/sec09/exam04/CallbackExample.java
@@ -0,0 +1,56 @@
+package sec09.exam04;
+
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.*;
+
+public class CallbackExample {
+    private ExecutorService executorService;
+
+    public CallbackExample() {
+        executorService = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+        );
+    }
+
+    private CompletionHandler<Integer, Void> callback = new CompletionHandler<Integer,Void>() {
+        @Override
+        public void completed(Integer result, Void attachment) {
+            System.out.println("completed() 실행: " + result);
+        }
+
+        @Override
+        public void failed(Throwable exc, Void attachment) { //Throwable: 예외 타입
+            System.out.println("failed() 실행: " + exc.toString());
+        }
+    };
+
+    public void doWork(final String x, final String y) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    int intX = Integer.parseInt(x);
+                    int intY = Integer.parseInt(y);
+                    int result = intX + intY;
+                    callback.completed(result, null);
+                } catch (NumberFormatException e) {
+                    callback.failed(e, null);
+                }
+            }
+        };
+        
+        executorService.submit(task);
+    }
+
+    public void finish() {
+        executorService.shutdown();
+    }
+
+    public static void main(String[] args) {
+        CallbackExample callbackExample = new CallbackExample();
+
+        callbackExample.doWork("3", "3");
+        callbackExample.doWork("3", "삼");
+        callbackExample.finish();
+    }
+}
