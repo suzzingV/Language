e55765c1e816f88cbd5fb148abae8932329cf091
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-08 02:46:41 +0900

    호출

--- chapter13/src/sec04/exam00/CompareMethodExample.java
+++ chapter13/src/sec04/exam00/CompareMethodExample.java
@@ -4,7 +4,7 @@ public class CompareMethodExample {
     public static void main(String[] args) {
         Pair<Integer, String> p1 = new Pair<Integer, String>(1, "사과");
         Pair<Integer, String> p2 = new Pair<Integer, String>(1, "사과");
-        boolean result1 = Util2.compare(p1, p2);
+        boolean result1 = Util2.<Integer, String>compare(p1, p2);
         if(result1) {
             System.out.println("논리적으로 동등한 객체입니다.");
         } else {
