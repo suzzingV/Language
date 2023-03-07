c55717c5a89a3071c49b91197e87a676d84141b0
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-08 02:46:13 +0900

    제네릭 메소드 호출

--- chapter13/src/sec04/exam00/Util2.java
+++ chapter13/src/sec04/exam00/Util2.java
@@ -1,7 +1,7 @@
 package sec04.exam00;
 
 public class Util2 {
-    public static <K, V> boolean compare(Pair<K, V> p1, Pair<k, V> p2) {
+    public static <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
         boolean keyCompare = p1.getKey().equals(p2.getKey());
         boolean valueCompare = p1.getValue().equals(p2.getValue());
         return keyCompare && valueCompare;
