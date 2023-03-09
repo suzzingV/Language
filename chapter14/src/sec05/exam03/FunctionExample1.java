c323c54f21730b14c61a75986478cbfba9f9fd1a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-09 16:07:49 +0900

    Function 함수적 인터페이스

---
+++ chapter14/src/sec05/exam03/FunctionExample1.java
@@ -0,0 +1,36 @@
+package exam03;
+
+import java.util.Arrays;
+import java.util.function.*;
+
+public class FunctionExample1 {
+    public static List<Student> list = Arrays.asList(
+        new Student("홍길동", 90, 96);
+        new Student("신용권", 95, 93);
+    );
+
+    public static void printString(Function<Student, String> function) {
+        for(Student student: list) {
+            System.out.println(function.apply(student) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void printInt(ToIntFunction<Student> function) {
+        for(Student student: list) {
+            System.out.println(function.applyAsInt(student) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("[학생 이름]");
+        printString( t -> return t.getName(););
+
+        System.out.println("[영어 점수]");
+        printInt(t -> return getEnglishScore(););
+        
+        System.out.println("[수학 점수]");
+        printInt(t -> return getMathScore(););
+    }
+}
