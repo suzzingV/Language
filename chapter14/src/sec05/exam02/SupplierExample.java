5ad0c23c5ab463b470344fe3da8d0a8fb27be3d7
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-09 15:54:37 +0900

    Supplier 함수적 인터페이스

---
+++ chapter14/src/sec05/exam02/SupplierExample.java
@@ -0,0 +1,15 @@
+package exam02;
+
+import java.util.function.*
+;
+public class SupplierExample {
+    public static void main(String[] args) {
+        IntSupplier intSupplier = () -> {
+            int num = (int) (Math.random() * 6) + 1;
+            return num;
+        };
+
+        int num = intSupplier.getAsInt();
+        System.out.println("´«ÀÇ ¼ö: " + num);
+    }
+}
