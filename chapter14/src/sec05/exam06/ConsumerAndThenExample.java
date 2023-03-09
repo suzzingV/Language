97230c95b524c5c681793e141cc8e3d5ac03471d
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-09 21:02:06 +0900

    Consumer의 andThen()

---
+++ chapter14/src/sec05/exam06/ConsumerAndThenExample.java
@@ -0,0 +1,18 @@
+package exam06;
+
+import java.util.function.*;
+
+public class ConsumerAndThenExample {
+    public static void main(String[] args) {
+        Consumer<Member> consumerA = (m) -> {
+            System.out.println("consumerA: " + m.getName());
+        };
+
+        Consumer<Member> consumerB = (m) -> {
+            System.out.println("consumerB: " + m.getId());
+        };
+
+        Consumer<Member> consumerAB = consumerA.andThen(consumerB);
+        consumerAB.accept(new Member("ȫ�浿", "hong", null));
+    }
+}
