746396f5436a34e25e5755884f736c9c0b5306b2
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-09 16:18:51 +0900

    Function 함수적 인터페이스

---
+++ chapter14/src/sec05/exam03/FunctionExample2.java
@@ -0,0 +1,29 @@
+package exam03;
+
+import java.util.Arrays;
+import java.util.function.ToIntFunction;
+import java.util.List;
+
+public class FunctionExample2 {
+    private static List<Student> list = Arrays.asList(
+        new Student("홍길동", 90, 96),
+        new Student("신용권", 95, 93)
+    );
+
+    public static double avg(ToIntFunction<Student> function) {
+        int sum = 0;
+        for(Student student : list) {
+            sum += function.applyAsInt(student);
+        }
+        double avg = sum / list.size();
+        return avg;
+    }
+
+    public static void main(String[] args) {
+        double englishAvg = avg(t -> t.getEnglishScore());
+        System.out.println("영어 평균 점수: " + englishAvg);
+
+        double mathAvg = avg(t -> t.getMathScore());
+        System.out.println("수학 평균 점수: " + mathAvg);
+    }
+}
