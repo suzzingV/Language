29801b79433970225b467e3734126b63b2cb4864
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-13 02:17:24 +0900

    LinkedList, ArrayList 시간 비교

---
+++ chapter15/src/sec02/exam03/LinkedListExample.java
@@ -0,0 +1,27 @@
+package sec02.exam03;
+
+import java.util.*;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        List<String> list1 = new LinkedList<String>();
+        List<String> list2 = new ArrayList<String>();
+
+        long startTime;
+        long endTime;
+
+        startTime = System.nanoTime();
+        for(int i = 0; i < 10000; i++) {
+            list1.add(0, String.valueOf(i));
+        }
+        endTime = System.nanoTime();
+        System.out.println("LinkedList 걸린 시간: " + (endTime - startTime));
+        
+        startTime = System.nanoTime();
+        for(int i = 0; i < 10000; i++) {
+            list2.add(0, String.valueOf(i));
+        }
+        endTime = System.nanoTime();
+        System.out.println("ArrayList 걸린 시간: " + (endTime - startTime));
+    }
+}
