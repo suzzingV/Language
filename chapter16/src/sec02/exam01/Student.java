e94dc7c29c2df11d5ea005846731d786e1eb882f
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-18 21:17:19 +0900

    Student 클래스

---
+++ chapter16/src/sec02/exam01/Student.java
@@ -0,0 +1,14 @@
+package sec02.exam01;
+
+public class Student {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getString() { return name; }
+    public int getScore() { return score; }
+}
