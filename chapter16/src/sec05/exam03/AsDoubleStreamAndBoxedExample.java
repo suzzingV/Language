13f46a35616d42a5fc6ac05e6724c53acef73872
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-19 16:16:27 +0900

    정렬

--- chapter16/src/sec05/exam03/AsDoubleStreamAndBoxedExample.java
+++ chapter16/src/sec05/exam03/AsDoubleStreamAndBoxedExample.java
@@ -9,15 +9,14 @@ public class AsDoubleStreamAndBoxedExample {
 
         IntStream intStream = Arrays.stream(intArray);
         intStream
-            .asDoubleStream();
-            //.forEach(d -> System.out.println(d));
+            .asDoubleStream()
+            .forEach(d -> System.out.println(d));
         
         System.out.println();
 
-        //intStream = Arrays.stream(intArray); //다시 정의 안해주면 그 뒤를 이으려고 함
+        intStream = Arrays.stream(intArray); //스트림 한번 작동하면 끝
         intStream
-            //.boxed()
-            //.forEach(obj -> System.out.println(obj.intValue()));
-            .forEach(d -> System.out.println(d));
+            .boxed()
+            .forEach(obj -> System.out.println(obj.intValue()));
     }
 }
