da62f31b4b0aefba9562990f414900b940c52877
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-19 15:52:12 +0900

    스트림 다시 정의 안해줬을 경우

--- chapter16/src/sec05/exam03/AsDoubleStreamAndBoxedExample.java
+++ chapter16/src/sec05/exam03/AsDoubleStreamAndBoxedExample.java
@@ -9,14 +9,15 @@ public class AsDoubleStreamAndBoxedExample {
 
         IntStream intStream = Arrays.stream(intArray);
         intStream
-            .asDoubleStream()
-            .forEach(d -> System.out.println(d));
+            .asDoubleStream();
+            //.forEach(d -> System.out.println(d));
         
         System.out.println();
 
-        intStream = Arrays.stream(intArray);
+        //intStream = Arrays.stream(intArray); //다시 정의 안해주면 그 뒤를 이으려고 함
         intStream
-            .boxed()
-            .forEach(obj -> System.out.println(obj.intValue()));
+            //.boxed()
+            //.forEach(obj -> System.out.println(obj.intValue()));
+            .forEach(d -> System.out.println(d));
     }
 }
