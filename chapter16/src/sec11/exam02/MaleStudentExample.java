adeba8030c83794af76913f31b1a8083dd67fe08
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-03-20 15:43:41 +0900

    사용자 정의 컨테이너에 수집하기

---
+++ chapter16/src/sec11/exam02/MaleStudentExample.java
@@ -0,0 +1,24 @@
+package sec11.exam02;
+
+import java.util.Arrays;
+import java.util.List;
+
+import sec11.exam01.Student;
+
+public class MaleStudentExample {
+    public static void main(String[] args) {
+        List<Student> totalList = Arrays.asList(
+            new Student("홍길동", 10, Student.Sex.MALE),
+            new Student("김수애", 6, Student.Sex.FEMALE),
+            new Student("신용권", 10, Student.Sex.MALE),
+            new Student("박수미", 6, Student.Sex.FEMALE)
+        );
+
+        MaleStudent maleStudent = totalList.stream()
+            .filter(s -> s.getSex() == Student.Sex.MALE)
+            .collect(MaleStudent :: new, MaleStudent :: accumulate, MaleStudent :: combine);
+        
+        maleStudent.getList().stream()
+            .forEach(s -> System.out.println(s.getName()));
+    }
+}
